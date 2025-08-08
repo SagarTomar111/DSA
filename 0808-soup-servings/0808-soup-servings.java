f2cfe3b5f3e0055bb2dc78c6214f89d0380664a5
SagarTomar <sagartomar7204920@gmail.com>
2025-08-08 23:00:08 +0530

    Time: 0 ms (100%), Space: 41 MB (98.8%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.java
+++ 0808-soup-servings/0808-soup-servings.java
@@ -1,30 +1,16 @@
 class Solution {
-    HashMap<String,Double> map ;
+    
     public double soupServings(int n) {
-        if(n>4500)
-        {
-            return 1.0;
-        }
-        n = (int) Math.ceil(n / 25.0);
-        map = new HashMap<>();
-        return solve(n,n);
+        if(n>4800) return 1;
+         n=(int)Math.ceil(n*1.0/25);
+        double dp[][]= new double[n+1][n+1];
+        return helper(n,n,dp);
     }
-    public double solve(int a , int b)
-    {
-        if(a<=0 && b<=0)
-            return 0.5;
-        if(a<=0)
-            return 1.0;
-        if(b<=0)
-            return 0.0;
-
-        String key = a + "*" + b ;
-        if(map.containsKey(key))
-        {
-            return map.get(key);
-        }
-        double ans = (solve(a-4,b) + solve(a-3,b-1) + solve(a-2,b-2) + solve(a-1,b-3))*0.25;
-        map.put(key , ans);
-        return ans;
+    double helper(int a,int b,double dp[][]){
+        if(a<=0 && b<=0) return 0.5;
+        if(b<=0) return 0;
+        if(a<=0) return 1;
+        if(dp[a][b]>0) return dp[a][b];
+        return dp[a][b]=0.25*(helper(a-4,b,dp)+helper(a-3,b-1,dp)+helper(a-2,b-2,dp)+helper(a-1,b-3,dp));
     }
 }
